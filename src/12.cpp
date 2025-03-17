43fce0293d1b56516ac69bc96097397dffb4119c
avrilamelie719 <avrilamelie719@gmail.com>
2025-03-17 06:16:37 +0000

    Add 12.cpp

---
+++ src/12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    double x = 3.14;
+    double y = 2.718;
+    double z = pow(x,y);
+    cout << "The value of z is: " << z << endl;
+    return 0;
+}
